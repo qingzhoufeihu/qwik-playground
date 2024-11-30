ecaa7a129f0ec7f507cf2a3263790032abca135e
Jagger <jiangyujian@duck.com>
2024-11-30 16:31:16 +0800

    feat: use link instead of a

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,32 +1,23 @@
 import { component$ } from "@builder.io/qwik";
 import { QwikLogo } from "../icons/qwik";
 import styles from "./header.module.css";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <header class={styles.header}>
       <div class={["container", styles.wrapper]}>
         <div class={styles.logo}>
-          <a href="/" title="qwik">
+          <Link href="/" title="qwik">
             <QwikLogo height={50} width={143} />
-          </a>
+          </Link>
         </div>
         <ul>
           <li>
-            <a
-              href="https://qwik.dev/docs/components/overview/"
-              target="_blank"
-            >
-              Docs
-            </a>
+            <Link href="/demo/flower">flower</Link>
           </li>
           <li>
-            <a
-              href="https://qwik.dev/examples/introduction/hello-world/"
-              target="_blank"
-            >
-              Examples
-            </a>
+            <Link href="/demo/todolist">todolist</Link>
           </li>
           <li>
             <a
